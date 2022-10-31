9c45f4be5991d675ed6ab9aa587c3c031bd9c325
Amal Babu <amalb0220@gmail.com>
2022-10-31 23:13:19 +0530

    validation completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,38 @@ function sumbitData(event) {
     event.preventDefault()
     let error = true
     let name = document.getElementById("name")
+    let email= document.getElementById("email")
+    let subject=document.getElementById("subject")
+    
     let nameError = document.getElementById("nameError")
+    let emailError = document.getElementById("emailError")
+    let subjectError = document.getElementById("subjectError")
     console.log(name.value);
     if (name.value === '') {
         error = false;
-        nameError.innerHTML = "Please Enter Your Name"
-    } else {
-        
+        nameError.innerHTML = "Please enter your name"
+    }else {  
         nameError.innerHTML = ""
-
     }
+    if (email.value === '') {
+        error = false;
+        emailError.innerHTML = "Please enter your email"
+    }else {  
+        emailError.innerHTML = ""
+    }
+
+    if (subject.value === '') {
+        error = false;
+        subjectError.innerHTML = "Please enter the subject"
+    }else {
+        subjectError.innerHTML = ""
+     }
+     if (message.value === '') {
+        error = false;
+        messageError.innerHTML = "Please enter the message"
+    }else {
+        messageError.innerHTML = ""
+     }
 
     if (error===true) {
         called()
